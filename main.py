3c2dfa09e38d301a46e1cb643c345e0a170f2bd8
Jason Heddings <jheddings@users.noreply.github.com>
2020-11-14 12:25:47 -0700

    UI improvements.

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import ui
 import setgen
 
-################################################################################
-def gen_button_pressed(sender):
-    set_length = int(app['size_text'].text)
-
-    #for set_num in range(num_sets):
-    set = builder.build_set(set_length)
-
-    set_table = app['set_table']
-    data_source = ui.ListDataSource(items=set)
-
-    set_table.data_source = data_source
-    set_table.reload_data()
-
-################################################################################
-## MAIN ENTRY
-
 # XXX make items editable?
 conf = setgen.load_config('setgen.yaml')
 builder = setgen.Builder(conf['items'])
 
+################################################################################
+def gen_button_pressed(sender):
+    set_length = int(form_size_text.text)
+    set = builder.build_set(set_length)
+
+    data_source = ui.ListDataSource(items=set)
+
+    form_set_table.data_source = data_source
+    form_set_table.reload_data()
+
+################################################################################
+## MAIN ENTRY
+
 app = ui.load_view()
+form_size_text = app['size_text']
+form_set_table = app['set_table']
+
 app.present('sheet')
