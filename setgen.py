98719fa4aaea2e6b8ef8366f44ed0e8aab1814df
Jason Heddings <jheddings@users.noreply.github.com>
2020-11-14 10:15:12 -0700

    Refactoring to improve reusability.

--- setgen.py
+++ setgen.py
@@ -2,6 +2,17 @@ import random
 import os
 
 ################################################################################
+def parse_args():
+    import argparse
+
+    argp = argparse.ArgumentParser(description='setgen: random set generator')
+
+    argp.add_argument('--config', default='setgen.yaml',
+                       help='configuration file (default: setgen.yaml)')
+
+    return argp.parse_args()
+
+################################################################################
 def load_config(config_file):
     import yaml
 
@@ -11,7 +22,7 @@ def load_config(config_file):
         from yaml import Loader as YamlLoader
 
     if not os.path.exists(config_file):
-        print('!! config file does not exist: %s', config_file)
+        print(f'!! config file does not exist: {config_file}')
         return None
 
     with open(config_file, 'r') as fp:
@@ -20,29 +31,37 @@ def load_config(config_file):
     return conf
 
 ################################################################################
-def build_set(exercises, length):
-    priority = [1 / entry['priority'] for entry in exercises]
-    set = random.choices(exercises, priority, k=set_length)
-
-    # TODO don't repeat exercises in a set
-
-    return set
+class Builder(object):
+    
+    #---------------------------------------------------------------------------
+    def __init__(self, items):
+        self.items = items
+    
+    #---------------------------------------------------------------------------
+    def build_set(self, length, allow_repeat=False):
+        priority = [1 / entry['priority'] for entry in self.items]
+        set = random.choices(self.items, priority, k=length)
+    
+        # TODO don't repeat items in a set
+    
+        return set
     
 ################################################################################
 ## MAIN ENTRY
 
-conf = load_config('workout.yaml')
-exercises = conf['exercises']
-num_sets = conf['total_sets']
-set_length = conf['set_length']
-
-#print(sorted(exercises, key=lambda x: x['priority']))
-
-for loop in range(num_sets):
-    set = build_set(exercises, set_length)
-    print(f'== Set {loop+1} ==')
-
-    for entry in set:
-        print(f'- {entry["name"]}')
-
-    print()
+if __name__ == "__main__":
+    args = parse_args()
+    conf = load_config(args.config)
+    builder = Builder(conf['items'])
+    
+    num_sets = conf['total_sets']
+    set_length = conf['set_length']
+    
+    for set_num in range(num_sets):
+        set = builder.build_set(set_length)
+        print(f'== Set {set_num+1} ==')
+    
+        for entry in set:
+            print(f'- {entry["title"]}')
+    
+        print()
