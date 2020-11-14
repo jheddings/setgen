821394842f746626ce9c337cb3c5094767b17862
Jason Heddings <jheddings@users.noreply.github.com>
2020-11-14 12:25:31 -0700

    Corrected duplicates when allow_repeat is False.

--- setgen.py
+++ setgen.py
@@ -32,36 +32,51 @@ def load_config(config_file):
 
 ################################################################################
 class Builder(object):
-    
+
     #---------------------------------------------------------------------------
     def __init__(self, items):
         self.items = items
-    
+
     #---------------------------------------------------------------------------
     def build_set(self, length, allow_repeat=False):
-        priority = [1 / entry['priority'] for entry in self.items]
-        set = random.choices(self.items, priority, k=length)
+        priority = [entry['priority'] for entry in self.items]
+
+        # use built in functions for generating random sets where possible...
+        if allow_repeat:
+            return random.choices(self.items, priority, k=length)
+
+        # generated a weighted list for sorting
+        for item in self.items:
+            item['__order'] = random.random() * item['priority']
+            
+        population = sorted(self.items, key=lambda x: x['__order'], reverse=True)
+
+        return population[:length]
+
+################################################################################
+def main(conf):
+    num_sets = conf.get('total_sets', 1)
+    set_length = conf.get('set_length', 3)
+    allow_repeat = conf.get('allow_repeat', False)
+
+    items = conf.get('items')
+    builder = Builder(items)
+
+    for set_num in range(num_sets):
+        set = builder.build_set(set_length, allow_repeat=allow_repeat)
+
+        if num_sets > 1:
+            print(f'== Set {set_num+1} ==')
     
-        # TODO don't repeat items in a set
+        for entry in set:
+            print(f'- {entry["title"]}')
     
-        return set
-    
+        print()
+
 ################################################################################
 ## MAIN ENTRY
 
 if __name__ == "__main__":
     args = parse_args()
     conf = load_config(args.config)
-    builder = Builder(conf['items'])
-    
-    num_sets = conf['total_sets']
-    set_length = conf['set_length']
-    
-    for set_num in range(num_sets):
-        set = builder.build_set(set_length)
-        print(f'== Set {set_num+1} ==')
-    
-        for entry in set:
-            print(f'- {entry["title"]}')
-    
-        print()
+    main(conf)
